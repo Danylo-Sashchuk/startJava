a3e1109d20b0658289430f6e2f19da785181c9af
Danylo-Sashchuk <danylo.sashchuk@gmail.com>
2023-01-29 11:47:26 -0500

    Added full calculator functionality

--- Lesson_1/Calculator.java
+++ Lesson_1/Calculator.java
@@ -1,9 +1,9 @@
 public class Calculator {
     public static void main(String[] args) {
-        int num1 = 5;
-        int num2 = 7;
-        char sign = '%';
-        int result = 0;
+        int num1 = 2;
+        int num2 = 10;
+        char sign = '^';
+        int result = 1;
         if (sign == '+') {
             result = num1 + num2;
         } else if (sign == '-') {
@@ -14,6 +14,10 @@ public class Calculator {
             result = num1 / num2;
         } else if (sign == '%') {
             result = num1 % num2;
+        } else {
+            for (int i = 0; i < num2; i++) {
+                result *= num1;
+            }
         }
         System.out.printf("%d %c %d = %d", num1, sign, num2, result);
     }
