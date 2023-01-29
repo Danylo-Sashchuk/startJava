3a364e1a72da6e04f7edc3a765f5890269d7b958
Danylo-Sashchuk <danylo.sashchuk@gmail.com>
2023-01-29 11:43:51 -0500

    Added all operators, but pow

--- Lesson_1/Calculator.java
+++ Lesson_1/Calculator.java
@@ -1,5 +1,20 @@
 public class Calculator {
     public static void main(String[] args) {
-        System.out.println("Initialization...");
+        int num1 = 5;
+        int num2 = 7;
+        char sign = '%';
+        int result = 0;
+        if (sign == '+') {
+            result = num1 + num2;
+        } else if (sign == '-') {
+            result = num1 - num2;
+        } else if (sign == '*') {
+            result = num1 * num2;
+        } else if (sign == '/') {
+            result = num1 / num2;
+        } else if (sign == '%') {
+            result = num1 % num2;
+        }
+        System.out.printf("%d %c %d = %d", num1, sign, num2, result);
     }
 }
