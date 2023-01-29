077d558b9461afe4fa04cb1c64303050f359a4db
Danylo-Sashchuk <danylo.sashchuk@gmail.com>
2023-01-29 14:29:50 -0500

    Added game

--- Lesson_1/GuessTheNumber.java
+++ Lesson_1/GuessTheNumber.java
@@ -1,5 +1,18 @@
 public class GuessTheNumber {
     public static void main(String[] args) {
-        System.out.println("Intitialization...");
+        int computersNumber = 25;
+        int playersNumber = 78;
+        while (playersNumber != computersNumber) {
+            if (playersNumber < computersNumber) {
+                System.out.printf("Число %d меньше того, что загадал компьютер.\n", playersNumber);
+            } else {
+                System.out.printf("Число %d больше того, что загадал компьютер.\n", playersNumber);
+            }
+            playersNumber++;
+            if (playersNumber > 100) {
+                playersNumber -= 100;
+            }
+        }
+        System.out.printf("\tВы победили! Компьютер загадал число %d!\n", computersNumber);
     }
 }
