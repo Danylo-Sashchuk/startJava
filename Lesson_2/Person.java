5c552b37382883545fe35c177b5393df3d8ca492
Danylo-Sashchuk <danylo.sashchuk@gmail.com>
2023-02-03 01:37:14 -0500

    Lesson 2 & Person class added

---
+++ Lesson_2/Person.java
@@ -0,0 +1,22 @@
+public class Person {
+
+    String gender = "Male";
+    double height = 1.75;
+    double weight = 75;
+    int age = 22;
+
+    void move() {
+    }
+
+    void sit() {
+    }
+
+    void run() {
+    }
+
+    void speak() {
+    }
+
+    void learnJava() {
+    }
+}
