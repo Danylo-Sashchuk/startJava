b29fc39214a4fee6aeb895a7133fadd0df7ff42f
Danylo-Sashchuk <danylo.sashchuk@gmail.com>
2023-02-04 09:07:08 -0500

    Added getters and setters in Wolf and WolfTest

--- Lesson_2/Wolf.java
+++ Lesson_2/Wolf.java
@@ -1,28 +1,73 @@
 public class Wolf {
 
-    String sex;
-    String name;
-    String color;
-    double weight;
-    int age;
+    private String sex;
+    private String name;
+    private String color;
+    private double weight;
+    private int age;
 
-    void walk() {
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        if (age > 8) {
+            System.out.println("Некорректный возраст");
+        }
+        else {
+            this.age = age;
+        }
+    }
+
+    public void walk() {
         System.out.printf("%s идет.\n", name);
     }
 
-    void sit() {
+    public void sit() {
         System.out.printf("%s сидит.\n", name);
     }
 
-    void run() {
+    public void run() {
         System.out.printf("%s бежит.\n", name);
     }
 
-    void howl() {
+    public void howl() {
         System.out.printf("%s воет.\n", name);
     }
 
-    void hunter() {
+    public void hunter() {
         System.out.printf("%s охотиться.\n", name);
     }
 }
--- Lesson_2/WolfTest.java
+++ Lesson_2/WolfTest.java
@@ -1,14 +1,14 @@
 public class WolfTest {
     public static void main(String[] args) {
         Wolf myWolf = new Wolf();
-        myWolf.sex = "Кобель";
-        myWolf.name = "Хьюго";
-        myWolf.color = "Серый";
-        myWolf.weight = 50;
-        myWolf.age = 12;
+        myWolf.setSex("Кобель");
+        myWolf.setName("Хьюго");
+        myWolf.setColor("Серый");
+        myWolf.setWeight(50);
+        myWolf.setAge(5);
         System.out.printf("Вы видете перед собой %s - это %s %s, которому " + 
-                "%d лет и весит он %2.2f кг.\n", myWolf.name, myWolf.color, myWolf.sex,
-                myWolf.age, myWolf.weight);
+                "%d лет и весит он %2.2f кг.\n", myWolf.getName(), myWolf.getColor(), 
+                myWolf.getSex(), myWolf.getAge(), myWolf.getWeight());
         myWolf.walk();
         myWolf.sit();
         myWolf.run();
